876b283b966fd5e17a011ce9a5c6559d69a020ec
Jack Beckitt-Marshall <jackbm633@gmail.com>
2024-08-31 15:13:58 +0100

    feat: implemented exit builtin

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
-﻿Console.Write("$ ");
-await Console.Out.FlushAsync();
+﻿int lastExitCode = 0;
+Dictionary<string, Action<IEnumerable<string>>> builtins = new() { { "exit", Exit } };
 
-// Wait for user input
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+while (true)
+{
+    Console.Write("$ ");
+    await Console.Out.FlushAsync();
+
+    // Wait for user input
+    var command = Console.ReadLine()!.TrimEnd().Split(" ").Where(s => s.Length > 0);
+    
+    if (command.Any())
+    {
+        if (builtins.ContainsKey(command.First()))
+        {
+            builtins[command.First()].Invoke(command);
+        }
+        Console.WriteLine($"{command.First()}: command not found");
+    }
+}
+
+
+void Exit(IEnumerable<string> commandInput)
+{
+    if (commandInput.Count() > 1 && byte.TryParse(commandInput.ElementAt(1), out byte exitcode))
+    {
+        Environment.Exit(exitcode);
+    }
+    else
+    {
+        Environment.Exit(lastExitCode);
+    }
+}
