46ca04666bf9170e1bff8fd54fcbab58aed25567
Jack Beckitt-Marshall <jackbm633@gmail.com>
2024-08-31 14:33:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShellSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
